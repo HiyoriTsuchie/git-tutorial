5ec28e96e0e6c6eee6b122a312c3bf475dab4582
j2018 <j2018@git.epi.it.matsue-ct.jp>
2023-11-30 15:36:04 +0900

    modified

--- test-remote/fizzbuzz.rb
+++ test-remote/fizzbuzz.rb
@@ -1,5 +1,8 @@
-a = 1
-while a < 10
-  puts a
-  a += 1 # a = a + 1と同じ意味
+#円の面積を計算するメソッド
+def area_circle(radius)
+  area = radius * radius * 3.14
+  return area
 end
+
+#メソッドを呼び出す
+puts area_circle(2) #引数に2を指定
