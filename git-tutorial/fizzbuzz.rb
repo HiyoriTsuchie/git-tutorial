b6ea69ffa98458163b7510b2c837284a2037efe0
j2018 <j2018@git.epi.it.matsue-ct.jp>
2023-12-07 13:53:52 +0900

    1207

---
+++ git-tutorial/fizzbuzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz(max_num)
+
+  (1..max_num).each do |num|
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 7 == 0
+      puts "git"
+    else
+      puts num
+    end
+  end
+end
+
+puts 'いくつまで数えますか？'
+num = gets.to_i
+fizzbuzz(num)
+
